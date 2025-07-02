a2eebe85b65b45f00568ce6698451694b338306f
MrRobotRock <sousaeduarda250@gmail.com>
2025-07-01 21:15:56 -0300

    meu primeiro commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,76 @@
+// src/pages/Home.jsx
+
+import React from "react";
+import { Box, Typography, Container } from "@mui/material";
+import Navbar from "../componentes/Navbar";
+import "../componentes/bubbles.css"; 
+
+
+function home() {
+  return (
+    <>
+      {/* {barra do munu glass} */}
+      <Navbar />
+      <div className="bubbles">
+  {[...Array(25)].map((_, i) => (
+    <div
+  key={i}
+  className="bubble"
+  style={{
+    left: `${Math.random() * 100}%`,
+    width: `${25 + Math.random() * 25}px`,
+    animationDuration: `${6 + Math.random() * 8}s`,
+    animationDelay: `${Math.random() * 4}s`,
+  }}
+/>
+
+  ))}
+</div>
+
+
+
+      {/* IMAGEM DE FUNDO MAIS TEXTO CENTRALIZADO*/}
+      <Box
+        sx={{
+          height: "100vh",
+          backgroundImage: 'url("/images/Frutiger Aero Aqua!!!.jpg")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "#fff",
+          px: 2,
+          pt: "80px", // espaço para a navbar fixa
+        }}
+      >
+        <Container>
+          <Typography
+            variant="h3"
+            sx={{
+              fontWeight: "bold",
+              textShadow: "2px 2px 4px rgba(0,0,0,0.7)",
+              mb: 2,
+              fontFamily: "'Asap', sans-serif",
+            }}
+          >
+            frutiger aeroooo
+          </Typography>
+
+          <Typography
+            variant="h6"
+            sx={{
+              textShadow: "1px 1px 3px rgba(0,0,0,0.5)",
+              fontFamily: "'Asap', sans-serif",
+            }}
+          >
+            Bem-vindo ao Café do Amanhã, onde o aroma do café se mistura com a brisa do mar.
+          </Typography>
+        </Container>
+      </Box>
+    </>
+  );
+}
+
+export default home;
