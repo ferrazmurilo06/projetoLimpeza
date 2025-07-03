dcec68e0049c760b4c85659eea6b403264864df8
axiel404 <axielferreira774@gmail.com>
2025-07-03 16:17:40 -0300

    Adiciona pasta page  e atualiza dependências do React

---
+++ src/pages/CadastroPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CadastroPage() {
+    return (
+        <div>
+            <h1>Página de Cadastro</h1>
+
+        </div>
+    );
+}
+
+export default CadastroPage;
