4f1de1825786f21f9f8c746b7b3a56e555fffb18
axiel404 <axielferreira774@gmail.com>
2025-07-18 17:36:37 -0300

    Ajeitei um erro no arquivo App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import { expect, test } from '@jest/globals';
 
 test('renders learn react link', () => {
   render(<App />);
